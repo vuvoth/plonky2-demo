a103d9c62e9b05fb795a01959a48ba081636c0e3
Vu Vo <vu.voth@gmail.com>
2023-02-22 17:15:30 +0700

    demo

--- src/circuit.rs
+++ src/circuit.rs
@@ -24,24 +24,32 @@ pub struct Circuit {
 
 impl Circuit {
     pub fn make_circuit(&self) -> Result<(Target, Target, CircuitData<F, C, 2>)> {
-        let config = CircuitConfig::standard_recursion_zk_config();
+        // use standard config 
+        let config = CircuitConfig::standard_recursion_config();
+        // create builder from config
         let mut builder = CircuitBuilder::new(config);
 
-        // x ^ 3 + x + 5 = 35
-        // config circuit
+        
+        // formula x ^ 3 + x + 5 = output
+        // configure circuit
 
+        // we can assume target is node in circuit         
         let x = builder.add_virtual_target();
 
+        // target x^3 = x * x * x
         let x_cube = builder.cube(x);
+        // target sum_0 = x^3 + x 
         let sum_0 = builder.add(x_cube, x);
+        // target output = sum_0 + 5 = x^3 + x ^ 5  
         let output = builder.add_const(sum_0, F::from_canonical_u32(5));
         
-
+        // we set output is public
         builder.register_public_input(output);
 
         // build circuit
         let circuit = builder.build();
 
+        // output circuit and target
         Ok((x, output, circuit))
     }
 
@@ -51,12 +59,16 @@ impl Circuit {
         output: Target,
         circuit: CircuitData<F, C, 2>,
     ) -> Result<()> {
+        // partial witness is witness with input only
         let mut pw = PartialWitness::new();
+        // we set input value for target
         pw.set_target(output, self.output);
         pw.set_target(x, self.x);
         
+        // plonky2 auto compute full witness base on circuit
         let proof = circuit.prove(pw).unwrap();
 
+        // verify proof 
         circuit.verify(proof)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,18 @@ use plonky2::field::field_types::Field;
 mod circuit;
 
 fn main() {
+
+    // create circuit struct
+    // x = 5 
+    // verify x^3 +x + 5 = 35 
     let circuit = Circuit {
         x: F::from_canonical_u64(3),
         output: F::from_canonical_u64(35)
     };
 
+    // make circuit 
     let (x,  output, circuit_instance) = circuit.make_circuit().unwrap();
 
-
+    // prove and verify data
     circuit.create_and_verify_proof(x, output, circuit_instance).unwrap()
 }
